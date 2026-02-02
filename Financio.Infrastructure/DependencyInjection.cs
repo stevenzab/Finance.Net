2b321115ea73c44aca33a225bb1a3f364823b7f7
stevenzab <steven.zabandith@epitech.eu>
2026-02-02 22:30:33 +0100

    [ADD] infrastructure / setup mongoDB

---
+++ Financio.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,29 @@
+﻿using Financio.Domain.Settings;
+using Financio.Infrastructure.Common;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using MongoDB.Driver;
+
+namespace Financio.Infrastructure
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+        {
+            var mongoDbSettings = configuration
+                .GetSection("MongoDb")
+                .Get<MongoSettings>();
+
+
+            if (mongoDbSettings == null)
+                throw new InvalidOperationException("MongoDB settings are not configured properly.");
+
+            var mongoClient = new MongoClient(mongoDbSettings.ConnectionString);
+            var mongoDatabase = mongoClient.GetDatabase(mongoDbSettings.DatabaseName);
+
+            services.AddSingleton(mongoDatabase);
+            services.AddScoped<IBaseRepository, BaseRepository>();
+            return services;
+        }
+    }
+}
