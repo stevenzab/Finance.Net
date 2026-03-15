4c10c3fdcd1e6a256f470f7d24fe3a5f7aa80957
stevenzab <steven.zabandith@epitech.eu>
2026-03-15 14:20:49 +0100

    [FIX] method to use the dto instead of the model directly

--- src/Financio.Applications/Services/Transactions/ITransactionService.cs
+++ src/Financio.Applications/Services/Transactions/ITransactionService.cs
@@ -1,15 +1,16 @@
 namespace Financio.Applications.Services.Transactions
 {
+    using Financio.Domain.Dto;
     using Financio.Domain.Models;
     using System.Collections.Generic;
     using System.Threading.Tasks;
 
     public interface ITransactionService
     {
-        Task<IEnumerable<Transaction>> GetAllAsync();
-        Task<Transaction> GetByIdAsync(string id);
-        Task CreateAsync(Transaction transaction);
-        Task UpdateAsync(string id, Transaction transaction);
+        Task<IEnumerable<TransactionDto>> GetAllAsync();
+        Task<TransactionDto> GetByIdAsync(string id);
+        Task<TransactionDto> CreateAsync(TransactionDto transaction);
+        Task<TransactionDto> UpdateAsync(string id, TransactionDto transaction);
         Task DeleteAsync(string id);
     }
 }
--- src/Financio.Applications/Services/Transactions/TransactionService.cs
+++ src/Financio.Applications/Services/Transactions/TransactionService.cs
@@ -2,6 +2,8 @@ using System.Collections.Generic;
 using System.Threading.Tasks;
 using Financio.Domain.Models;
 using Financio.Applications.Services.Transactions.DataAccess;
+using Financio.Domain.Dto;
+using Financio.Domain.Mapping;
 
 namespace Financio.Applications.Services.Transactions
 {
@@ -14,14 +16,59 @@ namespace Financio.Applications.Services.Transactions
             _dataAccess = dataAccess;
         }
 
-        public Task CreateAsync(Transaction transaction) => _dataAccess.CreateAsync(transaction);
+        public async Task<TransactionDto> CreateAsync(TransactionDto dtoSource)
+        {
+            var transaction = new Transaction
+            {
+                Amount = dtoSource.Amount,
+                Type = (Domain.Models.TransactionType)dtoSource.Type,
+                Category = dtoSource.Category,
+                Description = dtoSource.Description,
+                Date = dtoSource.Date
+            };
+
+            await _dataAccess.CreateAsync(transaction);
+
+            dtoSource.TransactionId = transaction.Id;
+
+            return dtoSource;
+        } 
 
         public Task DeleteAsync(string id) => _dataAccess.DeleteAsync(id);
 
-        public Task<IEnumerable<Transaction>> GetAllAsync() => _dataAccess.GetAllAsync();
+        public async Task<IEnumerable<TransactionDto>> GetAllAsync()
+        {
+            var getAllTransactions = await _dataAccess.GetAllAsync();
 
-        public Task<Transaction> GetByIdAsync(string id) => _dataAccess.GetByIdAsync(id);
+            var mapToDto = getAllTransactions.Select(x => x.MapToDto()).ToList();
 
-        public Task UpdateAsync(string id, Transaction transaction) => _dataAccess.UpdateAsync(id, transaction);
+            return mapToDto;
+
+        }
+
+        public async Task<TransactionDto> GetByIdAsync(string id)
+        {
+            var transaction = await _dataAccess.GetByIdAsync(id);
+
+            var mapToDto = transaction.MapToDto();
+
+            return mapToDto;
+        }
+
+        public async Task<TransactionDto> UpdateAsync(string id, TransactionDto transaction)
+        {
+            var updateTransaction = new Transaction
+            {
+                Id = id,
+                Amount = transaction.Amount,
+                Type = (Domain.Models.TransactionType)transaction.Type,
+                Category = transaction.Category,
+                Description = transaction.Description,
+                Date = transaction.Date
+            };
+            await _dataAccess.UpdateAsync(id, updateTransaction);
+
+            return updateTransaction.MapToDto();
+        }
     }
 }
